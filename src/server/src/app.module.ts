18159c78057991f685bfd01152b7f04a003729ae
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-20 09:49:40 -0600

    Imported config to app

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { MicroDataModule } from './micro-data/micro-data.module';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [MicroDataModule],
+  imports: [MicroDataModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
