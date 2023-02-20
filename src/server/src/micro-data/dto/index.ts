8546bf0c61fb79c9719185b01d2623fdd520a680
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-20 10:48:27 -0600

    Made interfaces for dtos

---
+++ src/server/src/micro-data/dto/index.ts
@@ -0,0 +1,4 @@
+export { MicrocontollerCommandDto } from './microcontoller-command.dto';
+export { MicrocontollerMetadataDto } from './microcontroller-metadata.dto';
+export { MicroControllerDataPointDto } from './microcontroller-datapoint.dto';
+export { ReferenceValueDto } from './reference-value.dto';
