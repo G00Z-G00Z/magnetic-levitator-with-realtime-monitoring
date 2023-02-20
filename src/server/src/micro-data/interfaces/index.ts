174ae3f4e417955e9fbe874772b916d399a9cc54
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-20 10:48:36 -0600

    Made interfaces for client & microcontrollers

---
+++ src/server/src/micro-data/interfaces/index.ts
@@ -0,0 +1,2 @@
+export { Client } from './client.interface';
+export { Microcontroller } from './microcontroller.interface';
---
+++ src/server/src/micro-data/interfaces/microcontroller.interface.ts
@@ -0,0 +1,5 @@
+export interface Microcontroller {
+  id: string;
+  name: string;
+  unit: string;
+}
