fc081a874ddf415380740ff751ca76412ecd1493
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-20 11:06:38 -0600

    Removed id from socket connections

--- src/server/src/micro-data/interfaces/socket-connection.interface.ts
+++ src/server/src/micro-data/interfaces/socket-connection.interface.ts
@@ -3,13 +3,11 @@ import { Microcontroller } from './microcontroller.interface';
 import { Socket } from 'socket.io';
 
 export interface SocketClientConnetion {
-  id: string;
   client: Client;
   socket: Socket;
 }
 
 export interface SocketDeviceConnetion {
-  id: string;
   device: Microcontroller;
   socket: Socket;
 }
