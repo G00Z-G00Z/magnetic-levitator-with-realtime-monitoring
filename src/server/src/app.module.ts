c1d17d584def811286ef54feea4d7ace91a2cd0d
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-24 08:28:22 -0600

    Added prisma service

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MicroDataModule } from './micro-data/micro-data.module';
 import { Module } from '@nestjs/common';
+import { PrismaServiceService } from './prisma-service/prisma-service.service';
 
 @Module({
   imports: [MicroDataModule, ConfigModule.forRoot()],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaServiceService],
 })
 export class AppModule {}
