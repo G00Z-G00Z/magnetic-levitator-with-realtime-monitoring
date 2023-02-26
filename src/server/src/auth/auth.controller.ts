1db04a57a0c6b266deabeefb40b5417a11728bee
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 17:27:27 -0600

    Made auth guard

--- src/server/src/auth/auth.controller.ts
+++ src/server/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { LocalGuard } from './local.guard';
 import { LoginDto } from './dto';
 import { PublicUser } from '../users/interface';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalGuard)
   @Post('login')
   async login(
     @Body() loginDto: LoginDto,
