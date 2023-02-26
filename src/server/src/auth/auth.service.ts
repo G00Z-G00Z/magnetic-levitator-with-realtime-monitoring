c7720fdae5b3be8d0e54857dffa202cbd72be7de
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 09:38:16 -0600

    Made method to validate user

--- src/server/src/auth/auth.service.ts
+++ src/server/src/auth/auth.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { PublicUser } from '../users/interface'
+import { UsersService } from '../users/users.service'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly userService: UsersService) {}
+
+  async validateUser(email: string, pass: string): Promise<PublicUser | null> {
+    const user = await this.userService.findOneByEmail(email);
+    if (user && user.password === pass) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+}
