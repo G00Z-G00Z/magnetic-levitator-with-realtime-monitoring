d3fdf2c57de00eca88bedb27c95249b3daa8a419
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 17:34:37 -0600

    Added jwt sign method

--- src/server/src/auth/auth.service.ts
+++ src/server/src/auth/auth.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common'
-import { PublicUser } from '../users/interface'
-import { UsersService } from '../users/users.service'
+import { Injectable } from '@nestjs/common';
+import { JWTPayload } from './interfaces';
+import { JwtService } from '@nestjs/jwt';
+import { PublicUser } from '../users/interface';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, pass: string): Promise<PublicUser | null> {
     const user = await this.userService.findOneByEmail(email);
@@ -13,5 +18,16 @@ export class AuthService {
       return result;
     }
     return null;
+  }
+
+  async login(user: PublicUser) {
+    const payload: JWTPayload = {
+      sub: user.id,
+      email: user.email,
+      name: user.name,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
