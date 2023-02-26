287a8b7826466f3216815c030431f98555de97e8
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 09:19:57 -0600

    Fixed prisma imports

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -3,13 +3,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { DevicesModule } from './devices/devices.module';
-import { PrismaService } from './prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ ConfigModule.forRoot(), DevicesModule, AuthModule, UsersModule],
+  imports: [ ConfigModule.forRoot(), DevicesModule, AuthModule, UsersModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService,PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
