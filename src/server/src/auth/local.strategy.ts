4b566d3475b399015e7cc5b807f7d842d80055a4
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 10:14:32 -0600

    Added local strategy

--- src/server/src/auth/local.strategy.ts
+++ src/server/src/auth/local.strategy.ts
@@ -1,15 +1,18 @@
 import { AuthService } from './auth.service'
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common'
 import { PassportStrategy } from '@nestjs/passport'
 import { Strategy } from 'passport-local'
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
+  private logger: Logger = new Logger('LocalStrategy');
+
   constructor(private authService: AuthService) {
     super();
   }
 
   async validate(email: string, password: string): Promise<any> {
+    this.logger.debug(`validate: email=${email}, password=${password}`);
     const user = await this.authService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException('Invalid credentials');
