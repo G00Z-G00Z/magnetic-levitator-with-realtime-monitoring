6879e37f5b74f856eb02ecb67e3a36c9831698b8
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 09:04:30 -0600

    Updated app module with new modules

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -4,9 +4,11 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { DevicesModule } from './devices/devices.module';
 import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ ConfigModule.forRoot(), DevicesModule],
+  imports: [ ConfigModule.forRoot(), DevicesModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService,PrismaService],
 })
