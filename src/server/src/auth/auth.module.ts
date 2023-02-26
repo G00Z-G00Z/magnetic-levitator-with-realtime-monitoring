e2e7c9583bb3e40fa3fab3270be3aeda1fbd3659
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 09:38:10 -0600

    Imported users module

--- src/server/src/auth/auth.module.ts
+++ src/server/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { AuthService } from './auth.service'
 import { Module } from '@nestjs/common'
+import { UsersModule } from '../users/users.module'
 
 @Module({
   providers: [AuthService],
   exports: [AuthService],
+  imports: [UsersModule],
 })
 export class AuthModule {}
