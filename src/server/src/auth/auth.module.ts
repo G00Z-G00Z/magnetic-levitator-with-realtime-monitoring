864c297e74f9ae3f79e020c0fdfa33f2ea4e9c02
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 17:39:53 -0600

    Imported JWT Module

--- src/server/src/auth/auth.module.ts
+++ src/server/src/auth/auth.module.ts
@@ -1,13 +1,24 @@
-import { AuthService } from './auth.service'
-import { LocalStrategy } from './local.strategy'
-import { Module } from '@nestjs/common'
-import { PassportModule } from '@nestjs/passport'
-import { UsersModule } from '../users/users.module'
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { JwtModule } from '@nestjs/jwt';
+import { LocalStrategy } from './local.strategy';
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60m' },
+    }),
+  ],
+
   providers: [AuthService, LocalStrategy],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
