6912b220c1e4c27a979124d1c5ce17ea1fc6eb04
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-26 09:38:03 -0600

    Exported auth service

--- src/server/src/auth/auth.module.ts
+++ src/server/src/auth/auth.module.ts
@@ -1,7 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from './auth.service'
+import { Module } from '@nestjs/common'
 
 @Module({
-  providers: [AuthService]
+  providers: [AuthService],
+  exports: [AuthService],
 })
 export class AuthModule {}
