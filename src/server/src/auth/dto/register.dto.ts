c811f4f70de64e36f648ff71c74142599173638f
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-01 07:55:58 -0600

    Changed name of registerdto

--- src/server/src/auth/dto/register.dto.ts
+++ src/server/src/auth/dto/register.dto.ts
@@ -1,9 +1,11 @@
 import { IsString, MaxLength, MinLength } from 'class-validator';
 import { LoginDto } from './login.dto';
 
-export class RegisterUserDto extends LoginDto {
+export class RegisterDto extends LoginDto {
   @IsString()
   @MinLength(5)
   @MaxLength(20)
   name: string;
 }
+
+
