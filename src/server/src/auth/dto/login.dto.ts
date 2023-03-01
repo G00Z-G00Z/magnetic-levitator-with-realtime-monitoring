a2fa715b554c8f288306d9ba87c8916949a09565
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-01 07:34:08 -0600

    Added constrains to Login dto

--- src/server/src/auth/dto/login.dto.ts
+++ src/server/src/auth/dto/login.dto.ts
@@ -1,9 +1,12 @@
-import { IsEmail, IsString, IsStrongPassword } from 'class-validator';
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class LoginDto {
   @IsEmail()
+  @MaxLength(40)
   email: string;
 
   @IsString()
+  @MinLength(8)
+  @MaxLength(25)
   password: string;
 }
