a08d63c8471812d80afcd3db2b8d9bcff895919e
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-01 07:56:28 -0600

    Create user dto

--- src/server/src/users/dto/create-user.dto.ts
+++ src/server/src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, MaxLength, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @MaxLength(40)
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(25)
+  password: string;
+
+  @IsString()
+  @MinLength(5)
+  @MaxLength(20)
+  name: string;
+}
