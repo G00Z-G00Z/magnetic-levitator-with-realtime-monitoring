1171fe922997abe297207ab1b11ba769f526ffc7
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-03 08:41:06 -0600

    Corrected types in device controller

--- src/server/src/devices/devices.controller.ts
+++ src/server/src/devices/devices.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { DevicesService } from './devices.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CreateDeviceDto } from './dto/create-device.dto';
+import { DevicesService } from './devices.service';
 import { UpdateDeviceDto } from './dto/update-device.dto';
 
 @Controller('devices')
 export class DevicesController {
   constructor(private readonly devicesService: DevicesService) {}
 
+  /**
+   * The user using this method, must be authenticated and authorized to create a device.Automatically, it will be connected to the user that created it.
+   */
   @Post()
   create(@Body() createDeviceDto: CreateDeviceDto) {
     return this.devicesService.create(createDeviceDto);
@@ -19,16 +30,16 @@ export class DevicesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return this.devicesService.findOne(+id);
+    return this.devicesService.findOne(id);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDeviceDto: UpdateDeviceDto) {
-    return this.devicesService.update(+id, updateDeviceDto);
+    return this.devicesService.update(id, updateDeviceDto);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.devicesService.remove(+id);
+    return this.devicesService.remove(id);
   }
 }
