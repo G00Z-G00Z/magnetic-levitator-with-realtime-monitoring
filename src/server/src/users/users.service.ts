b6c8a1fbb56f248b08a576d573f771c594d03c86
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-03 08:24:31 -0600

    Completed the rest of user REST methods

--- src/server/src/users/users.service.ts
+++ src/server/src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { CreateUserDto } from './dto/create-user.dto';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { PublicUser } from './interface';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
@@ -8,21 +9,22 @@ export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
-    const user = this.prisma.user.create({
+    const user = await this.prisma.user.create({
       data: {
         email: createUserDto.email,
         password: createUserDto.password,
         name: createUserDto.name,
       },
     });
-    return user;
+    return this.returnPublicUser(user);
   }
 
   async findAll() {
-    return await this.prisma.user.findMany({});
+    const users = await this.prisma.user.findMany({});
+    return users.map(this.returnPublicUser);
   }
 
-  async findOneById(id: string) {
+  private async findOneById(id: string) {
     const user = await this.prisma.user.findUnique({ where: { id } });
 
     if (!user) {
@@ -30,6 +32,10 @@ export class UsersService {
     }
 
     return user;
+  }
+
+  async findOneByIdPublic(id: string) {
+    return this.returnPublicUser(await this.findOneById(id));
   }
 
   async findOneByEmail(email: string) {
@@ -42,11 +48,38 @@ export class UsersService {
     return user;
   }
 
-  update(id: string, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const user = await this.findOneById(id);
+
+    const isActive =
+      updateUserDto.active === undefined ? user.active : updateUserDto.active;
+
+    const newUser = await this.prisma.user.update({
+      where: { id },
+      data: {
+        email: updateUserDto.email || user.email,
+        password: updateUserDto.password || user.password,
+        name: updateUserDto.name || user.name,
+        active: isActive,
+      },
+    });
+
+    return this.returnPublicUser(newUser);
   }
 
   async remove(id: string) {
-    return await this.prisma.user.delete({ where: { id } });
+    await this.prisma.user.delete({ where: { id } });
+    return {
+      msg: 'ok',
+    };
+  }
+
+  returnPublicUser(user: PublicUser) {
+    return {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      active: user.active,
+    };
   }
 }
