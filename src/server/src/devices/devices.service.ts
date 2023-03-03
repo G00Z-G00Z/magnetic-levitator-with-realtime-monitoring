7593322d3458d79bd6d084eb52e6318f04ceedff
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-03 08:40:51 -0600

    Updated device service

--- src/server/src/devices/devices.service.ts
+++ src/server/src/devices/devices.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
 import { CreateDeviceDto } from './dto/create-device.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { UpdateDeviceDto } from './dto/update-device.dto';
 
 @Injectable()
 export class DevicesService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createDeviceDto: CreateDeviceDto) {
     return 'This action adds a new device';
   }
 
   findAll() {
-    return `This action returns all devices`;
+    return this.prismaService.device.findMany({});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} device`;
+  async findOne(id: string) {
+    const device = await this.prismaService.device.findUnique({
+      where: { id },
+    });
+    if (!device) {
+      throw new NotFoundException('Device not found');
+    }
+    return device;
   }
 
-  update(id: number, updateDeviceDto: UpdateDeviceDto) {
+  update(id: string, updateDeviceDto: UpdateDeviceDto) {
     return `This action updates a #${id} device`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} device`;
+  async remove(id: string) {
+    await this.prismaService.device.delete({ where: { id } });
+    return {
+      msg: 'ok',
+    };
   }
 }
