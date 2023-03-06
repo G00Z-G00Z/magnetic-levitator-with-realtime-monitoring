5f68750b2e73a09b3e050b397edf8beb7b9274fd
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-06 10:34:32 -0600

    Jwt User decorator

---
+++ src/server/src/auth/jwt-user.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { JWTPayload } from "./interfaces";
+
+/**
+ * Takes a user from the request object in http
+ */
+export const JwtUser = createParamDecorator(
+    (data : unknown, ctx : ExecutionContext) => {
+        const request = ctx.switchToHttp().getRequest()
+        return request.user as JWTPayload
+    }
+)
