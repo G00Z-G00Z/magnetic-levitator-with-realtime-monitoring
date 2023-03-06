cd21d58ffd32813e43afd5ca0575b8569fc357c9
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-06 10:55:12 -0600

    Made all routes jwt protected by default

--- src/server/src/app.module.ts
+++ src/server/src/app.module.ts
@@ -1,16 +1,31 @@
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
-import { DevicesModule } from './devices/devices.module';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { DevicesModule } from './devices/devices.module';
+import { JwtAuthGuard } from './auth/jwt.guard';
+import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SeedModule } from './seed/seed.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ ConfigModule.forRoot(), DevicesModule, AuthModule, UsersModule, PrismaModule, SeedModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DevicesModule,
+    AuthModule,
+    UsersModule,
+    PrismaModule,
+    SeedModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
