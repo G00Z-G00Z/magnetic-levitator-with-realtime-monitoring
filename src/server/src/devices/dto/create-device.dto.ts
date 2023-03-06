a25223919800794f57025324bf53f88482c75c1c
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-06 16:28:35 -0600

    Create device dto

--- src/server/src/devices/dto/create-device.dto.ts
+++ src/server/src/devices/dto/create-device.dto.ts
@@ -1,1 +1,17 @@
-export class CreateDeviceDto {}
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateDeviceDto {
+  /**
+   * Name to identify the device
+   */
+  @IsString()
+  @MaxLength(20)
+  name: string;
+
+  /**
+   * Type of the device, refer to available types
+   */
+  @IsString()
+  @MaxLength(20)
+  type: string;
+}
