64d54549e00fe0096d8e50d18bcbc50a71bb79bc
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-06 17:02:49 -0600

    Imported device types module

--- src/server/src/devices/devices.module.ts
+++ src/server/src/devices/devices.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { DevicesService } from './devices.service';
 import { DevicesController } from './devices.controller';
+import { DeviceTypesModule } from 'src/device-types/device-types.module';
 
 @Module({
   controllers: [DevicesController],
   providers: [DevicesService],
+  imports: [DeviceTypesModule],
 })
 export class DevicesModule {}
