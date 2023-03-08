b441a3680589d245def091c004213af67f4d71ed
G00Z_G00Z <edygomezgg4@gmail.com>
2023-03-08 07:21:21 -0600

    Added secret for devices

--- src/server/src/auth/constants.ts
+++ src/server/src/auth/constants.ts
@@ -3,4 +3,8 @@ export const jwtConstants = {
    * This is secret for users
    */
   usersSecret: 'no me gusta el secreto',
+  /**
+   * This is secret from devices
+   */
+  devicesSecret: 'estoSecretode devices',
 };
